ffe08202420f7caa00946eac9f2195a9dd808057
mh <mh@immerda.ch>
2013-12-28 17:03:46 +0100

    reduce dependency on lsb modules, rather use the builtin facts

--- manifests/server/centos.pp
+++ manifests/server/centos.pp
@@ -1,11 +1,12 @@
+# centos stuff
 class rsyslog::server::centos inherits rsyslog::centos {
   include rsyslog::server::base
   File['/etc/sysconfig/rsyslog']{
     source => [ "puppet:///modules/site_rsyslog/server/config/CentOS/${::fqdn}/rsyslog",
-                "puppet:///modules/site_rsyslog/server/config/CentOS/rsyslog.${::lsbdistrelease}",
-                "puppet:///modules/site_rsyslog/server/config/CentOS/rsyslog",
-                "puppet:///modules/rsyslog/server/config/CentOS/rsyslog.${::lsbdistrelease}",
-                "puppet:///modules/rsyslog/server/config/CentOS/rsyslog" ],
+                "puppet:///modules/site_rsyslog/server/config/CentOS/rsyslog.${::operatingsystemmajrelease}",
+                'puppet:///modules/site_rsyslog/server/config/CentOS/rsyslog',
+                "puppet:///modules/rsyslog/server/config/CentOS/rsyslog.${::operatingsystemmajrelease}",
+                'puppet:///modules/rsyslog/server/config/CentOS/rsyslog' ],
   }
 }
 
