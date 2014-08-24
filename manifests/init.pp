38b5ccee6de7a7b66127d76c1820955ae0886d50
mh <mh@immerda.ch>
2014-08-24 17:51:38 +0200

    fixing module for debian

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,15 +5,16 @@
 # Marcel Härry haerry+puppet(at)puzzle.ch
 # Simon Josi josi+puppet(at)puzzle.ch
 #
-# This program is free software; you can redistribute 
-# it and/or modify it under the terms of the GNU 
-# General Public License version 3 as published by 
+# This program is free software; you can redistribute
+# it and/or modify it under the terms of the GNU
+# General Public License version 3 as published by
 # the Free Software Foundation.
 #
 
+# manage rsyslog
 class rsyslog {
   case $::operatingsystem {
-    centos: { include rsyslog::base }
-    default: { include rsyslog::centos }
+    centos: { include rsyslog::centos }
+    default: { include rsyslog::base }
   }
 }
