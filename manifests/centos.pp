6461dd8a8aaf5b29f4573e7908228fa5cb158254
mh <mh@immerda.ch>
2014-08-24 21:55:18 +0200

    fix centos specific stuff

--- manifests/centos.pp
+++ manifests/centos.pp
@@ -1,11 +1,14 @@
+# centos specific stuff
 class rsyslog::centos inherits rsyslog::base {
   file{'/etc/sysconfig/rsyslog':
-    source => [ "puppet:///modules/site_rsyslog/config/CentOS/${::fqdn}/rsyslog",
-                "puppet:///modules/site_rsyslog/config/CentOS/rsyslog.${::lsbdistrelease}",
-                "puppet:///modules/site_rsyslog/config/CentOS/rsyslog",
-                "puppet:///modules/rsyslog/config/CentOS/rsyslog.${::lsbdistrelease}",
-                "puppet:///modules/rsyslog/config/CentOS/rsyslog" ],
-     notify => Service['rsyslog'],
-     owner => root, group => 0, mode => 0644;
+    source  => ["puppet:///modules/site_rsyslog/config/CentOS/${::fqdn}/rsyslog",
+                "puppet:///modules/site_rsyslog/config/CentOS/rsyslog.${::operatingsystemmajrelease}",
+                'puppet:///modules/site_rsyslog/config/CentOS/rsyslog',
+                "puppet:///modules/rsyslog/config/CentOS/rsyslog.${::operatingsystemmajrelease}",
+                'puppet:///modules/rsyslog/config/CentOS/rsyslog' ],
+    notify  => Service['rsyslog'],
+    owner   => root,
+    group   => 0,
+    mode    => '0644';
   }
 }
