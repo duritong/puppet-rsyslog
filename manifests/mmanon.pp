c7cdcb5d228cb5cbb2b2fd80e6ce6c90a0707e67
mh <mh@immerda.ch>
2020-08-14 17:39:49 +0200

    shall be removed now

--- manifests/mmanon.pp
+++ manifests/mmanon.pp
@@ -1,9 +1,6 @@
 # enable anonymization
 class rsyslog::mmanon {
   rsyslog::confd{
-    # TODO: remove once this ran for a while
-    'mmanon':
-      ensure => absent;
     '00-mmanon':
       source    => 'puppet:///modules/rsyslog/config/mmanon.conf';
   }
