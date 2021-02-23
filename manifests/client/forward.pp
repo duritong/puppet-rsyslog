0c5bc6aa6a0a8b494f9c4c3ee9814a4f7f8976fa
mh <mh@immerda.ch>
2021-02-23 17:07:09 +0100

    proper naming

--- manifests/client/forward.pp
+++ manifests/client/forward.pp
@@ -3,9 +3,9 @@ define rsyslog::client::forward (
   Enum['present','absent'] $ensure = 'present',
   Optional[String] $content = undef,
   Optional[Array[String,1]] $source = [
-    "puppet:///modules/site_rsyslog/client/fwd/${facts['networking']['fqdn']}/${name}.conf",
-    "puppet:///modules/site_rsyslog/client/fwd/${name}.conf",
-    "puppet:///modules/rsyslog/client/fwd/${name}.conf",
+    "puppet:///modules/site_rsyslog/client/forwards/${facts['networking']['fqdn']}/${name}.conf",
+    "puppet:///modules/site_rsyslog/client/forwards/${name}.conf",
+    "puppet:///modules/rsyslog/client/forwards/${name}.conf",
   ],
 ) {
   rsyslog::confd { "03-fwd-${name}":
