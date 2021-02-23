e6b4d3eb3ae006b0310b162b7770791801e9e484
mh <mh@immerda.ch>
2021-02-23 16:51:52 +0100

    give content precedence

--- manifests/confd.pp
+++ manifests/confd.pp
@@ -1,34 +1,34 @@
 # manage config snippet
-define rsyslog::confd(
+define rsyslog::confd (
   Enum['present','absent'] $ensure = 'present',
-  Optional[String]         $source = undef,
-  Optional[String]         $content = undef,
+  Optional[Variant[String[1],Array[String[1],1]]] $source = undef,
+  Optional[String] $content = undef,
 ) {
   if ($ensure == 'present') and !($source or $content) {
-    fail("Requires either source or content")
+    fail('Requires either source or content')
   }
-  include ::rsyslog
-  file{"/etc/rsyslog.d/${name}.conf":
+  include rsyslog
+  file { "/etc/rsyslog.d/${name}.conf":
     require => Package['rsyslog'],
     notify  => Service['rsyslog'],
   }
   if $ensure == 'present' {
-    File["/etc/rsyslog.d/${name}.conf"]{
+    File["/etc/rsyslog.d/${name}.conf"] {
       owner   => root,
       group   => 0,
       mode    => '0640',
     }
-    if $source {
-      File["/etc/rsyslog.d/${name}.conf"]{
-        source => $source,
+    if $content {
+      File["/etc/rsyslog.d/${name}.conf"] {
+        content => $content,
       }
     } else {
-      File["/etc/rsyslog.d/${name}.conf"]{
-        content => $content,
+      File["/etc/rsyslog.d/${name}.conf"] {
+        source => $source,
       }
     }
   } else {
-    File["/etc/rsyslog.d/${name}.conf"]{
+    File["/etc/rsyslog.d/${name}.conf"] {
       ensure => 'absent',
     }
   }
